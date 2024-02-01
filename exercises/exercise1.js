ff8c40af4e0e1fc72072c193a0521af8a5782a64
Koorsch <anek0001@stud.kea.dk>
2024-02-01 07:14:40 +0100

    small changes to JS and random stuff.

--- exercises/exercise1.js
+++ exercises/exercise1.js
@@ -1,17 +1,17 @@
 // Make a function that returns a randomly generated number between 0 and 100
 "use strict";
 const rButton = document.querySelector("#randomBtn");
-const rResult = document.querySelector("#randomResult");
-const rBox = document.querySelector("#randomBox");
+const rResult = document.querySelector("h2");
 
 //Function called from clicking rButton.
 //Values hardcoded in the EventListener function-call.
 //Show rBox, replace its empty value with a max generated number of 100 and do console.log().
 function randomNumber(value, result) {
-  rBox.style.display = "block";
   let random = Math.floor(Math.random() * value);
   console.log("Computer says: " + random);
-  result.innerHTML = random;
+  //I know I know, assignment says return HTML, but this is better practice to prevent XSS-attacks (or so the internet says)!
+  //This will return just the text contained.
+  result.textContent = "Random number generated: " + random;
 }
 // Eventlistener on button, fires on click
 // Parameters are the value of 100 and the selected result <span> where text is changed.

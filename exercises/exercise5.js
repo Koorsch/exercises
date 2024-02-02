1cb117678f71b971fb761e4177e7d42cc52cc9d9
AndreasEkvall <anek0001@stud.kea.dk>
2024-02-02 12:53:36 +0100

    updated area function

--- exercises/exercise5.js
+++ exercises/exercise5.js
@@ -5,19 +5,33 @@ const output2 = document.querySelector("h3");
 const inputHeight = document.querySelector("#inputFieldHeight");
 const inputWidth = document.querySelector("#inputFieldWidth");
 
+const area = document.querySelector("#area").addEventListener("click", () => {
+  let height = Number(inputHeight.value);
+  let width = Number(inputWidth.value);
+  let areal = calcArea(height, width);
+  output1.textContent = `Area: ${areal}m`;
+  output2.classList.add("hidden");
+});
+
 const meters = document.querySelector("#meters").addEventListener("click", () => {
   let height = Number(inputHeight.value);
   let width = Number(inputWidth.value);
   let squared = squareMeters(height, width);
   output1.textContent = `Height + Width: ${height + width}m`;
   output2.textContent = `Meters squared: ${squared}m²`;
+  output2.classList.remove("hidden");
 });
 const feet = document.querySelector("#feet").addEventListener("click", () => {
   let convertToFeet = Number(inputHeight.value) + Number(inputWidth.value);
   let sendFeet = metersToFeet(convertToFeet);
   output1.textContent = `Height + Width: ${sendFeet}ft`;
   output2.textContent = `Feet²: ${sendFeet * 2}ft²`;
+  output2.classList.remove("hidden");
 });
+
+function calcArea(height, width) {
+  return height * 2 + width * 2;
+}
 
 function squareMeters(height, width) {
   return height * width;
